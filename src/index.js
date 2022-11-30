ff03b1229062d60e867226a97ad8eae11ae8915d
mbugua <mbuguafaimba@gmail.com>
2022-11-30 14:49:27 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("new").addEventListener("click",(event)=>{
+    event.preventDefault()
+  });
+ 
 });
